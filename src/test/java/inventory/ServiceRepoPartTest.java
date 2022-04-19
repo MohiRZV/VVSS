97164d85aacd8845bdfb3f4e6be50244c1699d12
Sidorac Constantin-Radu <radu_sidorac@yahoo.com>
2022-04-19 17:17:47 +0300

    finished integration testing

--- src/test/java/inventory/ServiceRepoPartTest.java
+++ src/test/java/inventory/ServiceRepoPartTest.java
@@ -1,35 +1,30 @@
 package inventory;
 
 import inventory.model.InhousePart;
+import inventory.model.OutsourcedPart;
 import inventory.model.Part;
 import inventory.model.PartValidator;
-import inventory.model.Product;
 import inventory.repository.InventoryFileRepository;
 import inventory.service.InventoryService;
-import javafx.collections.FXCollections;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
 
-import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
 
-public class ServiceRepoTest {
-    private InventoryFileRepository inventoryFileRepository;
+public class ServiceRepoPartTest {private InventoryFileRepository inventoryFileRepository;
     private PartValidator partValidator;
     private InventoryService inventoryService;
-    private Part p1;
-    private Part p2;
+
 
     @Before
     public void setUp(){
         partValidator = mock(PartValidator.class);
         inventoryFileRepository = new InventoryFileRepository(partValidator);
         inventoryService = new InventoryService(inventoryFileRepository);
-        p1 = mock(Part.class);
-        p2 = mock(Part.class);
     }
-
 
     @Test
     public void test_add_part() throws Exception{
@@ -50,21 +45,8 @@ public class ServiceRepoTest {
 
     @Test
     public void test_delete_part() throws Exception{
-        Mockito.when(p1.getName()).thenReturn("name");
-        Mockito.when(p1.getPartId()).thenReturn(100);
-        Mockito.when(p1.getInStock()).thenReturn(2);
-        Mockito.when(p1.getMax()).thenReturn(3);
-        Mockito.when(p1.getMin()).thenReturn(1);
-        Mockito.when(p1.getPrice()).thenReturn(20.0);
-        Mockito.when(p1.toString()).thenReturn("O,100,name,20.0,2,1,3,ab");
-
-        Mockito.when(p2.getName()).thenReturn("name");
-        Mockito.when(p2.getPartId()).thenReturn(102);
-        Mockito.when(p2.getInStock()).thenReturn(2);
-        Mockito.when(p2.getMax()).thenReturn(3);
-        Mockito.when(p2.getMin()).thenReturn(1);
-        Mockito.when(p2.getPrice()).thenReturn(20.0);
-        Mockito.when(p2.toString()).thenReturn("I,102,name,20.0,2,1,3,1");
+        Part p1 = new OutsourcedPart(100, "name", 20.0, 2, 1, 3, "ab");
+        Part p2 = new InhousePart(102, "name", 20.0, 2, 1, 3, 1);
 
         inventoryFileRepository.addPart(p1);
         inventoryFileRepository.addPart(p2);
@@ -75,5 +57,4 @@ public class ServiceRepoTest {
         assert inventoryService.getAllParts().size() == currentSize - 2;
 
     }
-
 }
