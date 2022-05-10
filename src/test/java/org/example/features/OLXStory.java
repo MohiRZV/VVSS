2985c6c6c6a85f858d532ca34fe9163d1617269d
MohiRZV <rsalsigan@gmail.com>
2022-05-10 13:30:15 +0300

    get test data from json + invalid login test

--- src/test/java/org/example/features/OLXStory.java
+++ src/test/java/org/example/features/OLXStory.java
@@ -45,6 +45,23 @@ public class OLXStory {
     }
 
     @Test
+    public void test_login_invalid() throws InterruptedException {
+        webdriver.get("https://www.olx.ro/cont/?ref%5B0%5D%5Baction%5D=myaccount&ref%5B0%5D%5Bmethod%5D=pro");
+        webdriver.manage().window().maximize();
+        WebElement username=webdriver.findElement(By.id("userEmail"));
+        WebElement password=webdriver.findElement(By.id("userPass"));
+        WebElement login=webdriver.findElement(By.id("se_userLogin"));
+        WebElement acceptCookies=webdriver.findElement(By.id("onetrust-accept-btn-handler"));
+        username.sendKeys(jsonDataProvider.getValue("email"));
+        password.sendKeys(jsonDataProvider.getValue("password_invalid"));
+        acceptCookies.click();
+        // nu merge fara because yes
+        Thread.sleep(100);
+        login.click();
+        Assert.assertFalse(webdriver.findElements(By.id("fblogin")).isEmpty());
+    }
+
+    @Test
     public void open_olx_main_page_select_contul_meu_fill_credentials_press_login_should_open_user_page() {
         user.is_the_home_page();
         user.acceptCookies();
